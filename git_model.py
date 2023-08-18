595ae0c8238256ec1c8c1ce1f051857aed8cb0fa
Ernestas <ernestasvvv@gmail.com>
2023-08-18 13:15:37 +0300

    Added class Klientas

--- git_model.py
+++ git_model.py
@@ -4,6 +4,13 @@ from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 engine = create_engine("sqlite:///:memory:")
 Base = declarative_base()
 
+class Klientas(Base):
+    __tablename__ = 'klientas'
+    id = Column(Integer, primary_key=True)
+    f_name = Column("vardas", String)
+    email = Column("el_pastas", String, unique=True)
+
+
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
